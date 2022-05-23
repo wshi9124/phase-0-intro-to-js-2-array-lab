21893876670bf27437b07ddb8ddfe677f1c760a1
Willie Shi <wshi9124@gmail.com>
2022-05-23 17:42:15 -0400

    commit tree 99e5ac2f1d0ff54e2647bd17b8164ee17ee7857d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) { 
+    cats.push(name)
+}
+
+function  destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    return [...cats,name]
+} 
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat(name) {
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+}
